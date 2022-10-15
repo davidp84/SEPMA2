3af207a48a6da0826348d557118a07d30461ba33
s3858839 <61816032+Tomhouse107@users.noreply.github.com>
2022-10-15 22:23:30 +1000

    Uncomment password validation

--- Engine.java
+++ Engine.java
@@ -6,14 +6,14 @@ public class Engine {
 
 	// checks if password meets requirements
 	public boolean PasswordIsValid(String password) {
-//		if (password.length() < 20)
-//			return false;
-//
-//		String pattern = "(.*[a-z].*)(.*[A-Z].*)(.*[0-9].*)|"
-//				+ "(.*[A-Z].*)(.*[a-z].*)|(.*[a-z].*)(.*[0-9].*)(.*[A-Z].*)|" + "(.*[0-9].*)(.*[a-z].*)(.*[A-Z].*)";
-//
-//		if (!password.matches(pattern))
-//			return false;
+		if (password.length() < 20)
+			return false;
+
+		String pattern = "(.*[a-z].*)(.*[A-Z].*)(.*[0-9].*)|"
+				+ "(.*[A-Z].*)(.*[a-z].*)|(.*[a-z].*)(.*[0-9].*)(.*[A-Z].*)|" + "(.*[0-9].*)(.*[a-z].*)(.*[A-Z].*)";
+
+		if (!password.matches(pattern))
+			return false;
 
 		return true;
 	}
